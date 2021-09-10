2057a4039e0d449a1bc448eed1ee0d7d352a3e75
Igor Silveira <igor.nqs@outlook.com>
2021-09-10 19:41:21 -0300

    Curso de desenvolvimento web FullStack
    Neste curso irei aprender JavaScript, Ajax, Node, Vue, Next, Express e outros

---
+++ Fundamentos/inicio/desafio_area.js
@@ -0,0 +1,8 @@
+// PI * raio * raio
+
+const PI = 3.1415;
+let raio = 10;
+let areaCirc = PI * raio * raio;
+
+console.log(areaCirc);
+console.log(Math.PI);
---
+++ Fundamentos/inicio/sentenca2.js
@@ -0,0 +1,3 @@
+console.log("Ola Igor");
+console.log("Bem vindo");
+console.log("Até mais");
