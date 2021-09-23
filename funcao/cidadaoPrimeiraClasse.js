ef5f8fb2c6662e182a8c81aec8e0861175af6cac
Igor Silveira <igor.nqs@outlook.com>
2021-09-23 19:31:48 -0300

    Inicio do capitulo de funcao
    Iniciamos o capitulo focado em funções.

---
+++ funcao/cidadaoPrimeiraClasse.js
@@ -0,0 +1,13 @@
+// função em JS é first-class object ( citizens)
+// higher-order function
+//criar de forma literal
+
+ function fun1 () {}
+
+ // armazenar em uma variavel
+
+ const fun2 = function (){}
+
+ //armazenar em array
+
+ // função como parametro
